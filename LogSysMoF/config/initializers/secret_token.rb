992d40610e077737d9cb4a476fad611e021208e9
VIEIRATER <vieirater@vieirater-laptop.(none)>
2013-09-17 01:30:48 -0400

    Create MoF Daily Log System

---
+++ LogSysMoF/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LogSysMoF::Application.config.secret_key_base = '8c9f509569e55ec67f0efbb6805b8cc6564c9a7274692fe9ca2786de7558c49dd8433768e385698cc282c6e791d46c515cce14a3a5e34b6696e9f12925f59380'
---
+++ LogSysMoF/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LogSysMoF::Application.config.session_store :cookie_store, key: '_LogSysMoF_session'
